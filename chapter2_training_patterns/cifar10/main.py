a52ab916978233ce6e7ec49e1496cd6bb4346a1a
shibuiwilliam <shibuiyusuke@gmail.com>
2021-01-09 19:46:42 +0900

    update

--- chapter2_training_patterns/cifar10/main.py
+++ chapter2_training_patterns/cifar10/main.py
@@ -15,26 +15,26 @@ logger = logging.getLogger(__name__)
 def main():
     with mlflow.start_run() as r:
         base_dir = "/opt/data/"
-        # preprocess_run = mlflow.run(
-        #     "./preprocess",
-        #     "preprocess",
-        #     backend="local",
-        #     parameters={
-        #         "data": "cifar10",
-        #         "downstream": os.path.join(base_dir, "preprocess/"),
-        #     },
-        # )
-        # preprocess_run = mlflow.tracking.MlflowClient().get_run(preprocess_run.run_id)
+        preprocess_run = mlflow.run(
+            "./preprocess",
+            "preprocess",
+            backend="local",
+            parameters={
+                "data": "cifar10",
+                "downstream": os.path.join(base_dir, "preprocess/"),
+            },
+        )
+        preprocess_run = mlflow.tracking.MlflowClient().get_run(preprocess_run.run_id)
 
         train_run = mlflow.run(
             "./train",
             "train",
             backend="local",
             parameters={
-                "upstream": os.path.join(
-                    "/tmp/mlruns/0", "624b8c80539c4de998d203890841f204", "artifacts/downstream_directory"
-                ),
-                # "upstream": os.path.join("/tmp/mlruns/0", preprocess_run.run_id, "artifacts/downstream_directory"),
+                # "upstream": os.path.join(
+                #     "/tmp/mlruns/0", "624b8c80539c4de998d203890841f204", "artifacts/downstream_directory"
+                # ),
+                "upstream": os.path.join("/tmp/mlruns/0", preprocess_run.run_id, "artifacts/downstream_directory"),
             },
         )
         train_run = mlflow.tracking.MlflowClient().get_run(train_run.run_id)
