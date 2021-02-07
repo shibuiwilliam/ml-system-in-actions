0a7f687e73018b14e7ff307321c011f0dd553177
shibuiwilliam <shibuiyusuke@gmail.com>
2021-02-07 19:27:45 +0900

    update

--- chapter2_training/cifar10/preprocess/src/preprocess.py
+++ chapter2_training/cifar10/preprocess/src/preprocess.py
@@ -1,27 +1,19 @@
 import argparse
 import json
-import logging
 import os
 from distutils.dir_util import copy_tree
 
 import mlflow
-import torch
 import torchvision
-import torchvision.transforms as transforms
 from src.configurations import PreprocessConfigurations
 from src.extract_data import parse_pickle, unpickle
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-
-def get_raw_data(downstream_directory: str):
-    torchvision.datasets.CIFAR10(root=downstream_directory, train=True, download=True)
-    torchvision.datasets.CIFAR10(root=downstream_directory, train=False, download=True)
-
 
 def main():
-    parser = argparse.ArgumentParser(description="Make dataset", formatter_class=argparse.RawTextHelpFormatter)
+    parser = argparse.ArgumentParser(
+        description="Make dataset",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
 
     parser.add_argument(
         "--data",
@@ -46,38 +38,78 @@ def main():
     downstream_directory = args.downstream
 
     if args.cached_data_id:
-        cached_artifact_directory = os.path.join("/tmp/mlruns/0", args.cached_data_id, "artifacts/downstream_directory")
-        copy_tree(cached_artifact_directory, downstream_directory)
+        cached_artifact_directory = os.path.join(
+            "/tmp/mlruns/0",
+            args.cached_data_id,
+            "artifacts/downstream_directory",
+        )
+        copy_tree(
+            cached_artifact_directory,
+            downstream_directory,
+        )
     else:
-        train_output_destination = os.path.join(downstream_directory, "train")
-        test_output_destination = os.path.join(downstream_directory, "test")
-        cifar10_directory = os.path.join(downstream_directory, "cifar-10-batches-py")
+        train_output_destination = os.path.join(
+            downstream_directory,
+            "train",
+        )
+        test_output_destination = os.path.join(
+            downstream_directory,
+            "test",
+        )
+        cifar10_directory = os.path.join(
+            downstream_directory,
+            "cifar-10-batches-py",
+        )
 
         os.makedirs(downstream_directory, exist_ok=True)
         os.makedirs(train_output_destination, exist_ok=True)
         os.makedirs(test_output_destination, exist_ok=True)
         os.makedirs(cifar10_directory, exist_ok=True)
 
-        get_raw_data(downstream_directory=downstream_directory)
+        torchvision.datasets.CIFAR10(
+            root=downstream_directory,
+            train=True,
+            download=True,
+        )
+        torchvision.datasets.CIFAR10(
+            root=downstream_directory,
+            train=False,
+            download=True,
+        )
 
-        meta_train = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: []}
-        meta_test = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: []}
+        meta_train = {i: [] for i in range(10)}
+        meta_test = {i: [] for i in range(10)}
 
         for f in PreprocessConfigurations.train_files:
             rawdata = unpickle(file=os.path.join(cifar10_directory, f))
-            class_to_filename = parse_pickle(rawdata=rawdata, rootdir=train_output_destination)
+            class_to_filename = parse_pickle(
+                rawdata=rawdata,
+                rootdir=train_output_destination,
+            )
             for cf in class_to_filename:
                 meta_train[int(cf[0])].append(cf[1])
 
         for f in PreprocessConfigurations.test_files:
             rawdata = unpickle(file=os.path.join(cifar10_directory, f))
-            class_to_filename = parse_pickle(rawdata=rawdata, rootdir=test_output_destination)
+            class_to_filename = parse_pickle(
+                rawdata=rawdata,
+                rootdir=test_output_destination,
+            )
             for cf in class_to_filename:
                 meta_test[int(cf[0])].append(cf[1])
 
-        classes_filepath = os.path.join(downstream_directory, "classes.json")
-        meta_train_filepath = os.path.join(downstream_directory, "meta_train.json")
-        meta_test_filepath = os.path.join(downstream_directory, "meta_test.json")
+        classes_filepath = os.path.join(
+            downstream_directory,
+            "classes.json",
+        )
+        meta_train_filepath = os.path.join(
+            downstream_directory,
+            "meta_train.json",
+        )
+        meta_test_filepath = os.path.join(
+            downstream_directory,
+            "meta_test.json",
+        )
         with open(classes_filepath, "w") as f:
             json.dump(PreprocessConfigurations.classes, f)
         with open(meta_train_filepath, "w") as f:
@@ -85,7 +117,10 @@ def main():
         with open(meta_test_filepath, "w") as f:
             json.dump(meta_test, f)
 
-    mlflow.log_artifacts(downstream_directory, artifact_path="downstream_directory")
+    mlflow.log_artifacts(
+        downstream_directory,
+        artifact_path="downstream_directory",
+    )
 
 
 if __name__ == "__main__":
